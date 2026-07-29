923326a404dfde5263a15d40584c654a1e523fda
Ben Key <bkey3125@gmail.com>
2026-07-28 23:12:49 -0500

    Add admin settings

--- Avalon/Main/SettingsView.swift
+++ Avalon/Main/SettingsView.swift
@@ -15,11 +15,9 @@ struct SettingsView: View {
                     .foregroundStyle(.primaryText)
                     .font(.livvic(size: .subheading))
                 
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Server")
-                        .foregroundStyle(.secondaryText)
-                        .font(.livvic(size: .note))
-                    
+                adminSettingsView
+                
+                makeSection(title: "Server") {
                     HStack {
                         Text("server.therealpercival.com")
                             .foregroundStyle(.primaryText)
@@ -42,13 +40,8 @@ struct SettingsView: View {
                         // Go back to setup screen
                     }
                 }
-                .frame(maxWidth: .infinity)
                 
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Account")
-                        .font(.livvic(size: .note))
-                        .foregroundStyle(.secondaryText)
-                    
+                makeSection(title: "Account") {
                     HStack {
                         Text("Ben")
                             .foregroundStyle(.primaryText)
@@ -71,7 +64,6 @@ struct SettingsView: View {
                         // Go back to setup screen
                     }
                 }
-                .frame(maxWidth: .infinity)
             }
             .padding(16)
             .frame(maxWidth: .infinity)
@@ -80,6 +72,83 @@ struct SettingsView: View {
         .background {
             Color.fullBackground
                 .ignoresSafeArea()
+        }
+    }
+    
+    @ViewBuilder
+    private var adminSettingsView: some View {
+        makeSection(title: "Requests") {
+            makeUserRow(color: .red1, primaryText: "@ben.json", canAccept: true)
+            makeUserRow(color: .green1, primaryText: "@65472624657", canAccept: true)
+            makeUserRow(color: .blue1, primaryText: "@_shoe_", canAccept: true)
+        }
+        
+        makeSection(title: "Accepted") {
+            makeUserRow(color: .red1, primaryText: "Ben", secondaryText: "@ben.json")
+            makeUserRow(color: .green1, primaryText: "Drew", secondaryText: "@65472624657")
+            makeUserRow(color: .blue1, primaryText: "Thomas", secondaryText: "@_shoe_")
+        }
+    }
+    
+    private func makeSection(title: String, @ViewBuilder content: () -> some View) -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(title)
+                .foregroundStyle(.secondaryText)
+                .font(.livvic(size: .note))
+            
+            content()
+        }
+        .frame(maxWidth: .infinity)
+    }
+    
+    private func makeUserRow(
+        color: Color,
+        primaryText: String,
+        secondaryText: String? = nil,
+        canAccept: Bool = false
+    ) -> some View {
+        HStack(spacing: 8) {
+            color
+                .frame(width: 32, height: 32)
+                .clipShape(.circle)
+            
+            Text(primaryText)
+                .foregroundStyle(.primaryText)
+                .font(.livvic)
+            
+            if let secondaryText {
+                Text(secondaryText)
+                    .foregroundStyle(.tertiaryText)
+                    .font(.livvic)
+            }
+            
+            Spacer()
+            
+            HStack(spacing: 16) {
+                if canAccept {
+                    Button {
+                        // Allow user in
+                    } label: {
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundStyle(.green2)
+                            .font(.livvic)
+                    }
+                }
+                
+                Button {
+                    // Remove user
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundStyle(.red1)
+                        .font(.livvic)
+                }
+            }
+        }
+        .padding(12)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .overlay {
+            RoundedRectangle(cornerRadius: 10)
+                .strokeBorder(.tertiaryText)
         }
     }
 }
