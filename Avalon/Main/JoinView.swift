6ec418feb072ab7ede8cf260d6a3095ca3abc468
Ben Key <bkey3125@gmail.com>
2026-07-29 09:07:52 -0500

    Add join button

--- Avalon/Main/JoinView.swift
+++ Avalon/Main/JoinView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct JoinView: View {
+    private var joinMessage: String = "Join Game (5 in lobby)"
+    private var spectateGameMessage: String = "Spectate (5 in game)"
+    private var spectateLobbyMessage: String = "Spectate (lobby full)"
+    
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
@@ -16,7 +20,15 @@ struct JoinView: View {
                     
                     Spacer()
                     
-                    Text("Join View")
+                    AvalonButton(joinMessage) {
+                        // Go to game screen
+                    } trailingView: {
+                        StackedProfilePictures {
+                            Color.red1
+                            Color.green1
+                            Color.blue2
+                        }
+                    }
                     
                     Spacer()
                 }
